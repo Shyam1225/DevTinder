002626553e3eea7b2255db720885805a7506740a
Shyam1225 <damanihit@gmail.com>
2026-01-08 17:57:51 +0530

    Some changes according to Frontend

--- src/routes/user.js
+++ src/routes/user.js
@@ -54,7 +54,7 @@ userRouter.get("/user/feed", userAuth, async(req, res) => {
         const loggedInUser = req.user;
 
         const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 10;
+        let limit = parseInt(req.query.limit) || 10;
         limit = limit > 50 ? 50 : limit;
         const skip = (page-1)*limit;        
 
